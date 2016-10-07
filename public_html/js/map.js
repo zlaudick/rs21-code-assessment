0549f4a65ccfed6b46bbf80945e1749b2707ad47
Zac Laudick <zlaudick@nmsu.edu>
2016-10-07 11:37:06 -0600

    testing example map

---
+++ public_html/js/map.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiemxhdWRpY2siLCJhIjoiY2l0ejYxbDNzMGE4ZjMzcGxhamR2eHZ4ZiJ9.1tLKHpx2rejGNSMYjCYk1w';
+var map = L.mapbox.map('map', 'mapbox.streets')
+	.setView([40, -74.50], 9);
